390b950f526d41e50e982b04dd1f5c890dd164fb
Elliott Lapinel <lapinele@flip2.engr.oregonstate.edu>
2019-11-10 23:32:41 -0800

    added error statement

--- projects/lapinele/dominion/unittest1.c
+++ projects/lapinele/dominion/unittest1.c
@@ -17,12 +17,18 @@ int main()
 	int numPlayers=3;
 	struct gameState* G=newGame();
 	int randomSeed=2;
-	
+	int coins=0;	
 	int initializeGame(numPlayers,k,randomSeed,G);
+	coins=G->coins;
 	
 	
-
 	baronEffect(0,G);
+	int newcoins=G->coins;
+	if(newcoins <= coins)
+	{
+	printf("There has been an error with baron's coin function\n");
+	}
+	
 	baronEffect(1,G);
 	
 	
